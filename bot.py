6ce5fca30e34ba497e1df62a34588267c7b0e23c
Christopher <cyoun@chapman.edu>
2023-04-25 20:10:57 -0700

    feat: Replace basic discord client with bot that listens to commands

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f"Logged on as {self.user}!")
-
-    async def on_message(self, message):
-        print(f"Message from {message.author}: {message.content}")
+@commands.command()
+async def test(ctx: commands.Context, arg: str = None):
+    await ctx.send(f"Test command ran with arg: {arg}")
 
 
 def main():
@@ -22,8 +20,14 @@ def main():
     intents = discord.Intents.default()
     intents.message_content = True
 
-    client = MyClient(intents=intents)
-    client.run(discord_bot_token)
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+    bot_commands = [test]
+
+    for bot_command in bot_commands:
+        bot.add_command(bot_command)
+
+    bot.run(discord_bot_token)
 
 
 if __name__ == "__main__":

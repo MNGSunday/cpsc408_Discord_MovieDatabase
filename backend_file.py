b28414765a31e15bb61347790331f2cd24815609
Christopher <cyoun@chapman.edu>
2023-04-28 11:44:28 -0700

    fix: Fix test data population

--- backend_file.py
+++ backend_file.py
@@ -1,5 +1,6 @@
 from helper import helper
 from db_operations import db_operations
+import pandas as pd
 # python backend_file.py
 
 # Create db_ops object
@@ -10,14 +11,14 @@ db_ops = db_operations()
 
 # Clean the sample data files 
 #NOTE: Might need to get rid of the header row for each file??? Not sure if that will be an issue
-movies_data = helper.data_cleaner("Test_Data/Movies-TestData.csv")
-actors_data = helper.data_cleaner("Test_Data/Actors-TestData.csv")
-movieActors_data = helper.data_cleaner("Test_Data/MovieActor-TestData.csv")
-directors_data = helper.data_cleaner("Test_Data/Directors-TestData.csv")
-composers_data = helper.data_cleaner("Test_Data/Composers-TestData.csv")
-songs_data = helper.data_cleaner("Test_Data/Songs-TestData.csv")
-studios_data = helper.data_cleaner("Test_Data/Studios-TestData.csv")
-reviews_data = helper.data_cleaner("Test_Data/Reviews-TestData.csv")
+movies_data = pd.read_csv("Test_Data/Movies-TestData.csv")
+actors_data = pd.read_csv("Test_Data/Actors-TestData.csv")
+movieActors_data = pd.read_csv("Test_Data/MovieActor-TestData.csv")
+directors_data = pd.read_csv("Test_Data/Directors-TestData.csv")
+composers_data = pd.read_csv("Test_Data/Composers-TestData.csv")
+songs_data = pd.read_csv("Test_Data/Songs-TestData.csv")
+studios_data = pd.read_csv("Test_Data/Studios-TestData.csv")
+reviews_data = pd.read_csv("Test_Data/Reviews-TestData.csv")
 
 # Create the database's tables
 db_ops.create_database_tables()
@@ -26,48 +27,55 @@ db_ops.create_database_tables()
 #Populates all 8 tables with given sample data
 def populate_with_sample_data():
     # insert the data from the csv files
-    attribute_count = len(movies_data[0])
+    attribute_count = len(movies_data.columns)
     placeholders = ("%s," * attribute_count)[:-1]
     query = "INSERT INTO Movies VALUES(" + placeholders + ")"
-    db_ops.bulk_insert(query, movies_data)
+    movies_bulk_data = list(movies_data.itertuples(index=False, name=None))
+    db_ops.bulk_insert(query, movies_bulk_data)
 
-    attribute_count = len(actors_data[0])
+    attribute_count = len(actors_data.columns)
     placeholders = ("%s," * attribute_count)[:-1]
     query = "INSERT INTO Actors VALUES(" + placeholders + ")"
-    db_ops.bulk_insert(query, actors_data)
+    actors_bulk_data = list(actors_data.itertuples(index=False, name=None))
+    db_ops.bulk_insert(query, actors_bulk_data)
 
-    attribute_count = len(movieActors_data[0])
+    attribute_count = len(movieActors_data.columns)
     placeholders = ("%s," * attribute_count)[:-1]
     query = "INSERT INTO MovieActors VALUES(" + placeholders + ")"
-    db_ops.bulk_insert(query, movieActors_data)
+    movieActors_bulk_data = list(movieActors_data.itertuples(index=False, name=None))
+    db_ops.bulk_insert(query, movieActors_bulk_data)
 
-    attribute_count = len(directors_data[0])
+    attribute_count = len(directors_data.columns)
     placeholders = ("%s," * attribute_count)[:-1]
     query = "INSERT INTO Directors VALUES(" + placeholders + ")"
-    db_ops.bulk_insert(query, directors_data)
+    directors_bulk_data = list(directors_data.itertuples(index=False, name=None))
+    db_ops.bulk_insert(query, directors_bulk_data)
 
-    attribute_count = len(composers_data[0])
+    attribute_count = len(composers_data.columns)
     placeholders = ("%s," * attribute_count)[:-1]
     query = "INSERT INTO Composers VALUES(" + placeholders + ")"
-    db_ops.bulk_insert(query, composers_data)
+    composers_bulk_data = list(composers_data.itertuples(index=False, name=None))
+    db_ops.bulk_insert(query, composers_bulk_data)
 
-    attribute_count = len(songs_data[0])
+    attribute_count = len(songs_data.columns)
     placeholders = ("%s," * attribute_count)[:-1]
     query = "INSERT INTO Songs VALUES(" + placeholders + ")"
-    db_ops.bulk_insert(query, songs_data)    
+    songs_bulk_data = list(songs_data.itertuples(index=False, name=None))
+    db_ops.bulk_insert(query, songs_bulk_data)    
 
-    attribute_count = len(studios_data[0])
+    attribute_count = len(studios_data.columns)
     placeholders = ("%s," * attribute_count)[:-1]
     query = "INSERT INTO Studios VALUES(" + placeholders + ")"
-    db_ops.bulk_insert(query, studios_data)  
+    studios_bulk_data = list(studios_data.itertuples(index=False, name=None))
+    db_ops.bulk_insert(query, studios_bulk_data)  
 
-    attribute_count = len(reviews_data[0])
+    attribute_count = len(reviews_data.columns)
     placeholders = ("%s," * attribute_count)[:-1]
     query = "INSERT INTO Reviews VALUES(" + placeholders + ")"
-    db_ops.bulk_insert(query, reviews_data)  
+    reviews_bulk_data = list(reviews_data.itertuples(index=False, name=None))
+    db_ops.bulk_insert(query, reviews_bulk_data)  
 
     print("Sample data has been inserted correctly")
-    input("Press Enter to continue...")
 
 # Printing records for all tables
 def print_table():
@@ -152,8 +160,6 @@ def print_table():
 
 
 # MAIN CODE:
-try:
-    populate_with_sample_data()
-except:
-    print("Proceeding as usual...")
+populate_with_sample_data()
+
 db_ops.destructor()

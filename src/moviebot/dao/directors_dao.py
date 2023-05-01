7e62b298d76145abf5a704f53e3e0540fa2bee8e
Christopher <cyoun@chapman.edu>
2023-05-01 16:41:35 -0700

    feat: Define DirectorsDAO class

---
+++ src/moviebot/dao/directors_dao.py
@@ -0,0 +1,25 @@
+from typing import List
+
+from mysql.connector.abstracts import MySQLConnectionAbstract
+from mysql.connector.pooling import PooledMySQLConnection
+
+
+class DirectorsDAO:
+    def __init__(self, db: MySQLConnectionAbstract | PooledMySQLConnection):
+        self.db = db
+
+    def get_attributes(self) -> List[str]:
+        with self.db.cursor(named_tuple=True) as cursor:
+            cursor.execute("SELECT * FROM Directors LIMIT 1;")
+            cursor.fetchall()
+            return (
+                [column[0] for column in cursor.description]
+                if cursor.description
+                else []
+            )
+
+    def list(self):
+        with self.db.cursor(named_tuple=True) as cursor:
+            cursor.execute("SELECT * FROM Directors;")
+            return cursor.fetchall()
+

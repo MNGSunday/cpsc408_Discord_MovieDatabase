928195f1f92a8e39576650d8424c7bd5b25235e3
Christopher <cyoun@chapman.edu>
2023-05-01 13:53:53 -0700

    refactor: Replace discord.py with pycord

--- moviebot.py
+++ moviebot.py
@@ -6,14 +6,6 @@ class MovieBot(commands.Bot):
         super().__init__(*args, **kwargs)
         self._load_commands()
 
-    async def on_ready(self):
-        print("Bot is ready")
-        try:
-            synced = await self.tree.sync()
-            print(f"Synced {len(synced)} commands")
-        except Exception as e:
-            print(f"Failed to sync commands: {e}")
-
     def _load_commands(self):
         @self.command()
         async def test(ctx: commands.Context, table_name: str = None):

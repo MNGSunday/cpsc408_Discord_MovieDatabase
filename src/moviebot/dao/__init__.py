cb7d56ad19f0c35ff9d52694d66fb1544e9341c5
Christopher <cyoun@chapman.edu>
2023-05-01 17:39:05 -0700

    feat: Export SongsDAO class in dao module

--- src/moviebot/dao/__init__.py
+++ src/moviebot/dao/__init__.py
@@ -2,3 +2,4 @@ from .movies_dao import MoviesDAO
 from .actors_dao import ActorsDAO
 from .directors_dao import DirectorsDAO
 from .composers_dao import ComposersDAO
+from .songs_dao import SongsDAO

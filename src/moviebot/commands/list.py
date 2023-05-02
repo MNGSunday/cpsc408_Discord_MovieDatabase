8148e15a587bcc25ea3b5cd1b0529191012ba216
Christopher <cyoun@chapman.edu>
2023-05-01 21:50:29 -0700

    feat: Change list slash command group to single slash command

--- src/moviebot/commands/list.py
+++ src/moviebot/commands/list.py
@@ -1,4 +1,5 @@
 import discord
+from discord.commands.options import option
 from prettytable import PrettyTable
 from discord.ext import commands
 from moviebot.abstract_moviebot import AbstractMovieBot
@@ -8,90 +9,36 @@ class ListCommands(commands.Cog):
     def __init__(self, bot: AbstractMovieBot):
         self.bot = bot
 
-    list_command = discord.SlashCommandGroup(
+    @commands.slash_command(
         name="list", description="Lists records of what you want to see"
     )
-
-    @list_command.command(description="Lists movies")
-    async def movies(self, interaction: discord.Interaction):
-        movie_attributes = self.bot.movies_dao.get_attributes()
-        movies = self.bot.movies_dao.list()
-
+    @option(
+        "entity",
+        description="The entity you want to list",
+        choices=["movies", "actors", "directors", "composers", "songs", "studios"],
+    )
+    async def list_command(self, ctx: discord.ApplicationContext, entity: str):
         table = PrettyTable()
-        table.field_names = movie_attributes
-        table.add_rows(movies)
 
-        await interaction.response.send_message(
-            f"```\n{table}\n```",
-            ephemeral=True,
-        )
+        if entity == "movies":
+            table.field_names = self.bot.movies_dao.get_attributes()
+            table.add_rows(self.bot.movies_dao.list())
+        elif entity == "actors":
+            table.field_names = self.bot.actors_dao.get_attributes()
+            table.add_rows(self.bot.actors_dao.list())
+        elif entity == "directors":
+            table.field_names = self.bot.directors_dao.get_attributes()
+            table.add_rows(self.bot.directors_dao.list())
+        elif entity == "composers":
+            table.field_names = self.bot.composers_dao.get_attributes()
+            table.add_rows(self.bot.composers_dao.list())
+        elif entity == "songs":
+            table.field_names = self.bot.songs_dao.get_attributes()
+            table.add_rows(self.bot.songs_dao.list())
+        elif entity == "studios":
+            table.field_names = self.bot.studios_dao.get_attributes()
+            table.add_rows(self.bot.studios_dao.list())
+        else:
+            raise ValueError("Invalid entity")
 
-    @list_command.command(description="Lists actors")
-    async def actors(self, interaction: discord.Interaction):
-        actor_attributes = self.bot.actors_dao.get_attributes()
-        actors = self.bot.actors_dao.list()
-
-        table = PrettyTable()
-        table.field_names = actor_attributes
-        table.add_rows(actors)
-
-        await interaction.response.send_message(
-            f"```\n{table}\n```",
-            ephemeral=True,
-        )
-
-    @list_command.command(description="Lists directors")
-    async def directors(self, interaction: discord.Interaction):
-        director_attributes = self.bot.directors_dao.get_attributes()
-        directors = self.bot.directors_dao.list()
-
-        table = PrettyTable()
-        table.field_names = director_attributes
-        table.add_rows(directors)
-
-        await interaction.response.send_message(
-            f"```\n{table}\n```",
-            ephemeral=True,
-        )
-
-    @list_command.command(description="Lists composers")
-    async def composers(self, interaction: discord.Interaction):
-        composer_attributes = self.bot.composers_dao.get_attributes()
-        composers = self.bot.composers_dao.list()
-
-        table = PrettyTable()
-        table.field_names = composer_attributes
-        table.add_rows(composers)
-
-        await interaction.response.send_message(
-            f"```\n{table}\n```",
-            ephemeral=True,
-        )
-
-    @list_command.command(description="Lists songs")
-    async def songs(self, interaction: discord.Interaction):
-        song_attributes = self.bot.songs_dao.get_attributes()
-        songs = self.bot.songs_dao.list()
-
-        table = PrettyTable()
-        table.field_names = song_attributes
-        table.add_rows(songs)
-
-        await interaction.response.send_message(
-            f"```\n{table}\n```",
-            ephemeral=True,
-        )
-
-    @list_command.command(description="Lists studios")
-    async def studios(self, interaction: discord.Interaction):
-        studio_attributes = self.bot.studios_dao.get_attributes()
-        studios = self.bot.studios_dao.list()
-
-        table = PrettyTable()
-        table.field_names = studio_attributes
-        table.add_rows(studios)
-
-        await interaction.response.send_message(
-            f"```\n{table}\n```",
-            ephemeral=True,
-        )
+        await ctx.response.send_message(f"```\n{table}\n```", ephemeral=True)

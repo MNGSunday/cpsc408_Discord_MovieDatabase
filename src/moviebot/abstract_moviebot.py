d5790746122d45da1119a5dd6112b3291caf695d
Christopher <cyoun@chapman.edu>
2023-05-01 21:12:29 -0700

    feat: Define AbstractMovieBot class

---
+++ src/moviebot/abstract_moviebot.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+from mysql.connector.abstracts import MySQLConnectionAbstract
+from mysql.connector.pooling import PooledMySQLConnection
+
+from .dao import ActorsDAO, ComposersDAO, DirectorsDAO, MoviesDAO, SongsDAO, StudiosDAO
+
+
+class AbstractMovieBot(commands.Bot):
+    def __init__(
+        self, db: MySQLConnectionAbstract | PooledMySQLConnection, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.movies_dao = MoviesDAO(db)
+        self.actors_dao = ActorsDAO(db)
+        self.directors_dao = DirectorsDAO(db)
+        self.composers_dao = ComposersDAO(db)
+        self.songs_dao = SongsDAO(db)
+        self.studios_dao = StudiosDAO(db)

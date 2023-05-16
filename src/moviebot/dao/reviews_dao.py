d6fe593916ed75e2c6baad7093b24cbe5bb0c349
Christopher <cyoun@chapman.edu>
2023-05-16 16:33:57 -0700

    feat: Create ReviewsDAO class

---
+++ src/moviebot/dao/reviews_dao.py
@@ -0,0 +1,34 @@
+from mysql.connector.abstracts import MySQLConnectionAbstract
+from mysql.connector.pooling import PooledMySQLConnection
+
+from moviebot.dao.paginated_data import PaginatedData
+from moviebot.entities.review import Review
+
+
+class ReviewsDAO:
+    def __init__(self, db: MySQLConnectionAbstract | PooledMySQLConnection):
+        self.db = db
+
+    def count(self) -> int:
+        with self.db.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM Reviews;")
+            res = cursor.fetchone()
+            if res is None:
+                raise ValueError("No count returned")
+            return res[0]
+
+    def list(self, offset: int = 0, limit: int = 5) -> PaginatedData[Review]:
+        with self.db.cursor(named_tuple=True) as cursor:
+            cursor.execute(
+                "SELECT * FROM Reviews ORDER BY reviewID LIMIT %s, %s;", (offset, limit)
+            )
+            return PaginatedData[Review](
+                data=[
+                    Review.from_named_tuple(review_named_tuple)
+                    for review_named_tuple in cursor.fetchall()
+                ],
+                offset=offset,
+                limit=limit,
+                total=self.count(),
+                paginate=self.list,
+            )
